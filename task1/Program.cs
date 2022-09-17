494be7c19d76064d1f64bf319a3be5db79b434ec
ruslan <rus.pro94@bk.ru>
2022-09-17 17:11:35 +0500

    .\task1\

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Число в квадрате
+Console.WriteLine("Пожалуйста введите число : ");
+
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Число: {number}. Квадрат: {result}");
